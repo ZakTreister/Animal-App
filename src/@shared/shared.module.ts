4fb362ae13c3f808463dd93e1d9a9ceef75dbcba
unknown <zakkyster@gmail.com>
2020-07-31 12:35:19 +0300

    Remove unfinished directive

--- src/@shared/shared.module.ts
+++ src/@shared/shared.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { InputDirective } from './directives/input.directive';
-import { InputComponent } from './directives/input.component';
 import { TableComponent } from './components/table/table.component';
 
 
 @NgModule({
   declarations: [
-    InputDirective,
-    InputComponent,
     TableComponent
   ],
   exports: [
